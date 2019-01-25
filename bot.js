ed1fa4b3b03afc7108e79ced1522ab9e73744968
serkanaygun <47044674+serkanaygun@users.noreply.github.com>
2019-01-26 01:59:16 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,170 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const ayarlar = require('./ayarlar.json');
+const chalk = require('chalk');
+const fs = require('fs');
+const moment = require('moment');
+require('./util/eventLoader')(client);
+
+var prefix = ayarlar.prefix;
+
+const log = message => {
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] ${message}`);
+};
+client.on('ready', () => {
+  console.log(`Botunuz Olan ${client.user.tag}Sunucuya Giriş Yaptı Ve Artık Aktif!`);
+});
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir('./komutlar/', (err, files) => {
+  if (err) console.error(err);
+  log(`${files.length} komut yüklenecek.`);
+  files.forEach(f => {
+    let props = require(`./komutlar/${f}`);
+    log(`Yüklenen komut: ${props.help.name}.`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
+};
+
+client.on('guildBanAdd' , (guild, user) => {
+  let girişçıkış = guild.channels.find('name', 'giriş-çıkış');
+  if (!girişçıkış) return;
+ giriş-çıkış.send('**Baktım Da Sunucumda Çok Artist Davranıyormuşsun!** '+ user.username +'**Cezanı Kestim Ve Kesmeye Devam** :fist: :writing_hand:  :spy:' );
+});
+
+client.load = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
+};
+
+client.unload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      resolve();
+    } catch (e){
+      reject(e);
+    }
+  });
+};
+
+client.on('message', msg => {
+  if (msg.content.toLowerCase() === 'sa') {
+    msg.channel.sendMessage('Aleyküm Selam Hoşgeldin :sparkling_heart::sparkling_heart:');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content.toLowerCase() === 'merhaba') {
+    msg.channel.sendMessage('Merhaba Hoşgeldin :sparkling_heart::sparkling_heart:');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content.toLowerCase() === 'günaydın') {
+    msg.channel.sendMessage('Günaydın Dostum Kalkmasaydın Zahmet Etmişsin :sparkling_heart::sparkling_heart:');
+  }
+  if (!msg.content.startsWith(prefix)) {
+    return;
+  }
+  if (msg.content.toLowerCase() === prefix + 'kedi' ) {
+    msg.reply('Tek Bildiğim Kedi Sensin Senden Başka Kedi Tanımam Ben.')
+  }
+  if (msg.content.toLowerCase() === prefix + 'adam' ) {
+    msg.reply('Hadi İyisin Bak Yene Adamlıkta Favori Listemin Başında Sen Varsın :)')
+  }
+});
+
+client.elevation = message => {
+  if(!message.guild) {
+	return; }
+  let permlvl = 0;
+  if (message.member.hasPermission("BAN_MEMBERS")) permlvl = 2;
+  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+  if (message.author.id === ayarlar.sahip) permlvl = 4;
+  return permlvl;
+};
+
+var regToken = /[\w\d]{24}\.[\w\d]{6}\.[\w\d-_]{27}/g;
+
+client.on('warn', e => {
+  console.log(chalk.bgYellow(e.replace(regToken, 'that was redacted')));
+});
+
+client.on('error', e => {
+  console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
+});
+exports.run = (client, message, args) => {
+	let mesaj = args.slice(0).join(' ');
+	if (mesaj.length < 1) return message.reply('Yazmam için herhangi bir şey yazmalısın.');
+  message.delete();
+  message.channel.send(mesaj);
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: ['say', 'söyle'],
+  permLevel: 3
+};
+
+exports.help = {
+  name: 'yaz',
+  description: 'İstediğiniz şeyi bota yazdırır.',
+  usage: 'yaz [yazdırmak istediğiniz şey]'
+};
+module.exports = client => {
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
+  client.user.setStatus("online");
+  client.user.setGame(`RotaMüzik Senle`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Şu an ` + client.channels.size + ` adet kanala, ` + client.guilds.size + ` adet sunucuya ve ` + client.guilds.reduce((a, b) => a + b.memberCount, 0).toLocaleString() + ` kullanıcıya hizmet veriliyor!`);
+};
+module.exports = member => {
+    let username = member.user.username;
+    member.sendMessage('Selamlar,Değerli **' + username + '** Discord Sunucuma Hoşgeldin Burda Seninle İyi Vakit Geçirmek Dileğiyle İyi Eğlenceler **');
+
+};
+
+
+client.login(ayarlar.token);
---
+++ events/guildMemberAdd.js
@@ -0,0 +1,5 @@
+module.exports = member => {
+    let username = member.user.username;
+    member.sendMessage('Selamlar,Değerli **' + username + '** Discord Sunucuma Hoşgeldin :kissing_heart: #kurallar Kısmına Bakmayı Unutma :thinking: Keyifli Vakit Geçirmen Dileğiyle :wink:');
+
+};
